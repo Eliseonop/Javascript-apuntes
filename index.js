781802a7192b5d93e97e4fc91db0b12ada27ab9d
Eliseo Falcon <eliseonop@gmail.com>
2022-05-21 16:18:29 -0500

    ejercicio

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor s
+const nombre = 'Eliseo'
+const edad = 21
+
+const desarrollador = true
+
+const fechaNacimiento = new Date('December 22, 2000')
+
+const favoriteBook = {
+  title: 'Eloquent JavaScript 3rd Edition',
+  autor: 'Marijn Haverbeke',
+  date: new Date('2018'),
+  url: 'https://eloquentjavascript.net/'
+}
+console.log(nombre, edad, desarrollador, fechaNacimiento, favoriteBook)
