74fee919de947cc521b7666847a85de69d4776be
Eliseo Falcon <eliseonop@gmail.com>
2022-05-22 13:04:57 -0500

    ejercio de strings

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-// ejercicos de javascript
+// Crea un archivo JS que contenga las siguientes líneas
 
-// Crea los siguientes archivos JS:
+// - Una cadena de texto con tu Nombre
 
-// - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+// - Otra cadena de texto con tu Apellido
 
-// - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+// - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
 
-// - factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+// - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
 
-// npm run start > para correr archivos
+// - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+
+// - Una variable que contenga la primera letra del Nombre
+
+// - Otra variable que contenga la última letra del Apellido
+
+// - Una cadena de texto que elimine los espacios de la variable "estudiante"
+
+// - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
+
+const nombre = 'Eliseo'
+const apellido = 'Falcon'
+
+const estudiante = nombre + ' ' + apellido
+const estudianteMayus = estudiante.toUpperCase()
+
+const estudianteMinus = estudiante.toLowerCase()
+const numeroEstudiante = estudiante.length
+const primeraLetra = estudiante.charAt(0)
+
+const ultimaLetraApellido = apellido.charAt(apellido.length - 1)
+
+const estudianteSinEspacios = estudiante.replace(/\s/g, '')
+
+const estaNombreEnEstudiante = estudiante.includes(nombre)
