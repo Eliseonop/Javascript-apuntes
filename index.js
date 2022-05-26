443f46271a79c907e0661854c2924b43b6e37d12
Eliseo Falcon <eliseonop@gmail.com>
2022-05-25 23:23:13 -0500

    update

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-import { multiplica, suma } from './controller.js'
-import chalk from 'chalk'
-
-const Suma1 = suma(1, 2)
-
-const Suma2 = suma(4, 5)
-
-const green = chalk.bold.green
-const magenta = chalk.bold.magenta
-
-console.log(magenta('Suma1: '), green(Suma1))
-console.log(green(`Suma1: ${Suma2}`))
-
-// axios
-//   .get('https://pokeapi.co/api/v2/pokemon/ditto')
-//   .then(response => {
-//     console.log('Success')
-//     console.log(response.data)
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
+class Estudiante {
+  #nombre
+  #asignaturas = ['Javascript', 'HTML', 'CSS']
+  constructor (nombre) {
+    this.#nombre = nombre
+  }
+  obtenDatos () {
+    return {
+      nombre: this.#nombre,
+      asignaturas: this.#asignaturas
+    }
+  }
+}
+const estudiante_1 = new Estudiante('Gorka')
+console.log(estudiante_1.obtenDatos())
