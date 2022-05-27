e2f2f745f2e88455110b5c7be94a4eb0b955e36d
Eliseo Falcon <eliseonop@gmail.com>
2022-05-26 19:07:13 -0500

    update-debug

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-class Estudiante {
-  #nombre
-  #asignaturas = ['Javascript', 'HTML', 'CSS']
-  constructor (nombre) {
-    this.#nombre = nombre
+// Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+
+// - Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+
+function fibonacci (num) {
+  var lista = [1, 1]
+  for (var i = 2; i < num; i++) {
+    lista.push(lista[i - 2] + lista[i - 1])
   }
-  obtenDatos () {
-    return {
-      nombre: this.#nombre,
-      asignaturas: this.#asignaturas
-    }
-  }
+  return lista
 }
-const estudiante_1 = new Estudiante('Gorka')
-console.log(estudiante_1.obtenDatos())
+const lista = fibonacci(6)
+console.log(lista)
