f71ad71640ead6a730bdbe1edc98102d135764a3
Eliseo Falcon <eliseonop@gmail.com>
2022-05-28 21:01:44 -0500

    update

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-const nombre = 'Eliseo'
-const apellido = 'Falcon'
+const parrafos = document.querySelectorAll('.parrafo')
+const secciones = document.querySelectorAll('.seccion')
+const papelera = document.querySelectorAll('.papelera')
 
-const objectName = {
-  nombre,
-  apellido
-}
+parrafos.forEach(parrafo => {
+  // cuando comienza a arrastrar
+  parrafo.addEventListener('dragstart', e => {
+    console.log('Estoy arrastrando el párrafo' + parrafo.innerText)
+    parrafo.classList.add('dragging')
+    e.dataTransfer.setData('id', parrafo.id)
+    const imagen = document.querySelector('.imagen-fantasma')
+    e.dataTransfer.setDragImage(imagen, 0, 0)
+  })
+  // cuando deja de arrastrar
+  parrafo.addEventListener('dragend', () => {
+    parrafo.classList.remove('dragging')
+  })
+})
 
-sessionStorage.setItem('objectName', JSON.stringify(objectName))
+secciones.forEach(seccion => {
+  //mientras arrastra algo al parecer un hijo de esta seccion
+  seccion.addEventListener('dragover', e => {
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'copy'
+  })
+  //cuando suelta el elemento
+  seccion.addEventListener('drop', e => {
+    e.preventDefault()
+    const id_parrafo = e.dataTransfer.getData('id')
+    const parrafo = document.getElementById(id_parrafo)
+    seccion.appendChild(parrafo)
+    console.log(id_parrafo)
+  })
+})
 
-localStorage.setItem('objectName', JSON.stringify(objectName))
-
-document.cookie = `objectName=${JSON.stringify(objectName)}; max-age=120`
-
-// localStorage.setItem(
-//   'persona',
-//   JSON.stringify({ nombre: 'Eliseo', edad: '21' })
-// )
-// // console.log(JSON.parse(localStorage.getItem('persona')))
-
-// localStorage.removeItem('nombre')
-
-// sessionStorage.setItem('nombre', 'Copo')
-
-// document.cookie = 'nombre=Copo'
-
-// document.cookie = 'nombre=Copo; expires=' + new Date(2030, 1, 1).toUTCString()
-
-// console.log(document.cookie)
+papelera.forEach(papelera => {
+  papelera.addEventListener('dragover', e => {
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'copy'
+  })
+  papelera.addEventListener('drop', e => {
+    e.preventDefault()
+    const id_parrafo = e.dataTransfer.getData('id')
+    const parrafo = document.getElementById(id_parrafo)
+    parrafo.remove()
+    console.log(id_parrafo)
+  })
+})
